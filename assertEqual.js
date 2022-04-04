ee387a3115cbe6956fe1605532a38129b1eed9ee
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-04-04 16:00:38 -0400

    adding assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🤩🤩🤩 Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("😡😵‍💫😰 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("bootcamp", "Bootcamp");
+assertEqual(1, 3);
+assertEqual(1,);
+assertEqual();
